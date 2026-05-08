01e5477b42769eb1201c0df50bb039806d76095f
yazmin577 <yazminjara3344@gmail.com>
2026-05-08 09:10:20 -0600

    tablas

--- ExamenUnidad4/app/src/main/java/com/example/examenunidad4/AdminSQLite.java
+++ ExamenUnidad4/app/src/main/java/com/example/examenunidad4/AdminSQLite.java
@@ -1,4 +1,4 @@
-package com.example.examenunidad4;
+package com.example.practicabd;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
@@ -6,19 +6,24 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
-public class AdminSQLite extends SQLiteOpenHelper {
+public class AdminSqLite extends SQLiteOpenHelper {
 
-    public AdminSQLite(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+    public AdminSqLite(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table estudiantes(numcontrol int primary key, nombrealum text, telalum int)");
+        db.execSQL("create table estudiantes(numcontrol int primary key, nombrealum text, telalum text)");
+        db.execSQL("create table docentes(num_empleado int primary key, nombre text, direccion text)");
+        db.execSQL("create table materias(clave_ int primary key, nombre_materia text)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("drop table if exists estudiantes");
+        db.execSQL("drop table if exists docentes");
+        db.execSQL("drop table if exists materias");
+        onCreate(db);
     }
 }
